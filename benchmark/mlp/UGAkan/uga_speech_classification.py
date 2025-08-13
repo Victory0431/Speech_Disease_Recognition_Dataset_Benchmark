555c6d15dabb0e3e50c05554386860f9bce39ee9
Victory0431 <1951281196@qq.com>
2025-08-13 13:56:45 +0000

    UGA NORMAL MLP

--- benchmark/mlp/UGAkan/uga_speech_classification.py
+++ benchmark/mlp/UGAkan/uga_speech_classification.py
@@ -178,9 +178,9 @@ class UGADataset(BaseDataset):
         return features, labels
 
 # MLP模型 - 适应四分类任务
-class MLP(nn.Module):
+class MLP_huge(nn.Module):
     def __init__(self, input_dim, hidden_size=128, num_classes=4):
-        super(MLP, self).__init__()
+        super(MLP_huge, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_size)
         self.bn1 = nn.BatchNorm1d(hidden_size)
         self.relu = nn.ReLU()
@@ -200,6 +200,20 @@ class MLP(nn.Module):
         x = self.relu(x)
         x = self.dropout2(x)
         x = self.fc3(x)
+        return x
+
+# MLP模型
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_size=64,num_classes=4):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)  # 二分类输出
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
         return x
 
 # 训练和评估函数
